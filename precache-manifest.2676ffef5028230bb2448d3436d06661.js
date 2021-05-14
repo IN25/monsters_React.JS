37afaf0d4f9a0ca46a36648b91289ec9883996f2
Islam Nuryyev <islamn25@IN25-MacBook-Pro.local>
2021-05-14 21:38:41 +0500

    Updates

---
+++ precache-manifest.2676ffef5028230bb2448d3436d06661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eba3d1be96e8f2c82fe8",
+    "url": "/monsters_react/static/css/main.34340e6f.chunk.css"
+  },
+  {
+    "revision": "eba3d1be96e8f2c82fe8",
+    "url": "/monsters_react/static/js/main.56db4517.chunk.js"
+  },
+  {
+    "revision": "876065c10fce63b4f022",
+    "url": "/monsters_react/static/js/runtime~main.49d4535c.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/monsters_react/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "38c42e0744ea7f4604440ab2d1685f70",
+    "url": "/monsters_react/index.html"
+  }
+];
